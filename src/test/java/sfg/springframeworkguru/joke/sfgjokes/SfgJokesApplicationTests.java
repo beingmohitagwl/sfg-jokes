7fb3604ac9830a43516f3aa973cc9edf0f12e110
beingmohitagwl <beingmohitagwl@gmail.com>
2021-08-01 16:29:47 +0530

    First commit

---
+++ src/test/java/sfg/springframeworkguru/joke/sfgjokes/SfgJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.springframeworkguru.joke.sfgjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
